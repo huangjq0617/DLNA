f0e601d9b01f60f8e3e545c4956db7458c89fe37
huangjq0617 <hjq0617@gmail.com>
2014-04-05 18:22:38 +0800

    dmp 代码简化

--- clinkc/std/av/wrapper/objc/CyberLink/CGUpnpAvRenderer.m
+++ clinkc/std/av/wrapper/objc/CyberLink/CGUpnpAvRenderer.m
@@ -13,17 +13,6 @@
 #import "CGUpnpAVPositionInfo.h"
 #import "CGUpnpAvItem.h"
 
-#define ADD_CHILDNODE_TO_NODE(node, name, value) \
-{ \
-    if (value) \
-    { \
-        CgXmlNode *newNode = cg_xml_node_new(); \
-        cg_xml_node_setname(newNode, (name)); \
-        cg_xml_node_setvalue(newNode, (char *)[value UTF8String]); \
-        cg_xml_node_addchildnode(node, newNode); \
-    } \
-}
-
 @interface CGUpnpAvRenderer()
 @property (assign) int currentPlayMode;
 @end
@@ -77,38 +66,13 @@ enum {
 	if (!action)
 		return NO;
     
-    CgXmlNode *didl_node = cg_upnpav_didl_node_new();
-    CgString *currentUriMeta = cg_string_new();
-    
     CgXmlNode *item = cg_xml_node_new();
-    cg_xml_node_setname(item, "item");
-    cg_xml_node_setattribute(item, CG_UPNPAV_OBJECT_ID, ((char *)[[NSString stringWithFormat:@"%@", [avItem objectId]] UTF8String]) );
-    cg_xml_node_setattribute(item, CG_UPNPAV_OBJECT_PARENTID, ((char *)[[NSString stringWithFormat:@"%@", [[avItem parent] objectId]] UTF8String]) );
-    cg_xml_node_setattribute(item, CG_UPNPAV_OBJECT_RESTRICTED, ((char *)[[NSString stringWithFormat:@"%d", 1] UTF8String]) );
+    cg_xml_node_copy(item, avItem.cXmlNode);
     
+    CgXmlNode *didl_node = cg_upnpav_didl_node_new();
     cg_xml_node_addchildnode(didl_node, item);
     
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_TITLE, [avItem title]);
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_UPNPCLASS, [avItem upnpClass]);
-    
-    for (CGUpnpAvResource *resource in [avItem resources]) {
-        
-        CgXmlNode *res = cg_xml_node_new();
-        cg_xml_node_setname(res, "res");
-        cg_xml_node_setattribute(res, "protocolInfo", ((char *)[[NSString stringWithFormat:@"%@", [resource protocolInfo]] UTF8String]) );
-        if ([resource size]) {
-            cg_xml_node_setattribute(res, "size", ((char *)[[NSString stringWithFormat:@"%lld", [resource size]] UTF8String]) );
-        }
-        cg_xml_node_setattribute(res, "resolution", ((char *)[[NSString stringWithFormat:@"%dx%d",
-                                                               (int)[resource resolution].width, (int)[resource resolution].height] UTF8String]) );
-        cg_xml_node_setvalue(res, ((char *)[[resource url] UTF8String]) );
-        cg_xml_node_addchildnode(item, res);
-    }
-    
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_ALBUM, [avItem album]);
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_ALBUMARTURI, [avItem albumArtURI]);
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_ARTIST, [avItem artist]);
-    ADD_CHILDNODE_TO_NODE(item, CG_UPNPAV_OBJECT_DATE, [avItem date]);
+    CgString *currentUriMeta = cg_string_new();
     
 	[action setArgumentValue:@"0" forName:@"InstanceID"];
 	[action setArgumentValue:[avItem.resourceUrl absoluteString] forName:@"CurrentURI"];
@@ -117,6 +81,8 @@ enum {
     [urlMetaValue insertString:@CG_UPNP_XML_DECLARATION atIndex:0];
     
 	[action setArgumentValue:urlMetaValue forName:@"CurrentURIMetaData"];
+    
+    cg_xml_node_print(didl_node);
     
     cg_string_delete(currentUriMeta);
     cg_xml_node_delete(didl_node);
